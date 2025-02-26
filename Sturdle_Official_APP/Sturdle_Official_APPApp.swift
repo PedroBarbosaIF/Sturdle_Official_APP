5c45283b80ca3996696348270805510d86ffb7ba
PedroBarbosaIF <pedro.barbosa40@aluno.ifce.edu.br>
2025-02-26 14:19:25 -0300

    Initial Commit

---
+++ Sturdle_Official_APP/Sturdle_Official_APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sturdle_Official_APPApp.swift
+//  Sturdle_Official_APP
+//
+//  Created by found on 26/02/25.
+//
+
+import SwiftUI
+
+@main
+struct Sturdle_Official_APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Sturdle_Official_APPTests/Sturdle_Official_APPTests.swift
@@ -0,0 +1,17 @@
+//
+//  Sturdle_Official_APPTests.swift
+//  Sturdle_Official_APPTests
+//
+//  Created by found on 26/02/25.
+//
+
+import Testing
+@testable import Sturdle_Official_APP
+
+struct Sturdle_Official_APPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
